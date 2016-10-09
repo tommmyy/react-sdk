4809bb6834acbb7aafb3a2a6ad4071ad2334c5a5
tommmyy <tomaskonrady@gmail.com>
2016-10-09 20:50:53 +0200

    Fixes

--- run.js
+++ run.js
@@ -7,92 +7,94 @@ const chalk = require('chalk');
 /**
  * Find an automation script by its name. Given the following list of files:
  *
- *   ./node_modules/react-sdk-tools/scripts/build.js
- *   ./node_modules/react-sdk-tools/scripts/start.js
- *   ./scripts/start.js
+ *	 ./node_modules/start-react-tools/scripts/build.js
+ *	 ./node_modules/start-react-tools/scripts/start.js
+ *	 ./scripts/start.js
  *
  * It should return:
  *
- *   findScript('build') => './node_modules/react-sdk-tools/scripts/build.js'
- *   findScript('start') => './scripts/start.js'
- *   findScript('foo')   => null
+ *	 findScript('build') => './node_modules/start-react-tools/scripts/build.js'
+ *	 findScript('start') => './scripts/start.js'
+ *	 findScript('foo')	 => null
  */
 const findScript = (() => {
-  const files = [];
-  return name => {
-    if (!files.length) {
-      try {
-        files.push(...fs.readdirSync(path.join(process.cwd(), 'scripts'))
-          .map(file => path.join(process.cwd(), 'scripts', file)));
-      } catch (err) {
-        if (process.env.VERBOSE === 'true') {
-          console.log(`INFO: No scripts found in ${path.join(process.cwd(), 'scripts')}`);
-        }
-      }
-      try {
-        files.push(...fs.readdirSync(path.join(__dirname, 'scripts'))
-          .map(file => path.join(path.join(__dirname, 'scripts', file))));
-      } catch (err) {
-        console.warn('WARNING: Cannot find \'start-react-tools\' npm module.');
-      }
-    }
-    return files.find(x => path.basename(x) === `${name}.js`);
-  };
+	const files = [];
+	return (name) => {
+		if (!files.length) {
+			try {
+				files.push(...fs.readdirSync(path.join(process.cwd(), 'scripts'))
+					.map((file) => path.join(process.cwd(), 'scripts', file)));
+			} catch (err) {
+				if (process.env.VERBOSE === 'true') {
+					console.log(`INFO: No scripts found in ${path.join(process.cwd(), 'scripts')}`);
+				}
+			}
+			try {
+				files.push(...fs.readdirSync(path.join(__dirname, 'scripts'))
+					.map((file) => path.join(path.join(__dirname, 'scripts', file))));
+			} catch (err) {
+				console.warn('WARNING: Cannot find \'start-react-tools\' npm module.');
+			}
+		}
+		return files.find((x) => path.basename(x) === `${name}.js`);
+	};
 })();
 
 
 const findConfig = (() => {
-  let config;
-  return () => {
-    if (config) return config;
+	let config;
+	return () => {
+		if (config) return config;
 
-    try {
-      config = require(path.resolve(process.cwd(), './config.js'));
-    } catch (err) {
-      config = {};
-    }
+		try {
+			config = require(path.resolve(process.cwd(), './config.js'));
+		} catch (err) {
+			config = {};
+		}
 
-    const webpackConfig = process.env.NODE_ENV === 'development' ?
-    	require('./scripts/webpack-dev.config') :
-    	require('./scripts/webpack-prod.config');
+		const webpackConfig = process.env.NODE_ENV === 'development' ?
+			require('./scripts/webpack-dev.config') :
+			require('./scripts/webpack-prod.config');
 
-    if (typeof config.webpack === 'function') {
-      config.webpack = config.webpack(webpackConfig) || webpackConfig;
-    } else {
-      config.webpack = webpackConfig;
-    }
+		if (typeof config.webpack === 'function') {
+			config.webpack = config.webpack(webpackConfig) || webpackConfig;
+		} else {
+			config.webpack = webpackConfig;
+		}
 
-    return config;
-  };
+		return config;
+	};
 })();
 
 /**
  * Find and execute a script. For example: run('clean').then(() => console.log('Done!');
  *
- *   Starting 'clean'...
- *   Finished 'clean' after 45ms
- *   Done!
+ *	 Starting 'clean'...
+ *	 Finished 'clean' after 45ms
+ *	 Done!
  */
 function run(command) {
-  const script = findScript(command);
-  const config = findConfig();
+	const script = findScript(command);
+	const config = findConfig();
 
-  if (!script) {
-    console.error(`ERROR: File not found: scripts/${command}.js`);
-    process.exit(1);
-  }
+	if (!script) {
+		console.error(`ERROR: File not found: scripts/${command}.js`);
+		process.exit(1);
+	}
 
-  const start = new Date();
-  console.log(`Starting '${chalk.bold.gray(command)}'...`);
+	const start = new Date();
+	console.log(`Starting '${chalk.bold.gray(command)}'...`);
 
-  return Promise.resolve()
-  	.then(() => require(script)(config))
-  	.then(() => {
-	    console.log(
-	      `Finished '${chalk.bold.gray(command)}' after ` +
-	      `${chalk.bold(new Date().getTime() - start.getTime())}ms`
-    );
-  }, err => console.error(err.stack));
+	return Promise.resolve()
+		.then(() => require(script)(config))
+		.then(() => {
+			console.log(
+				`Finished '${chalk.bold.gray(command)}' after ` +
+				`${chalk.bold(new Date().getTime() - start.getTime())}ms`
+			);
+		},
+		(err) => console.error(err.stack)
+	);
 }
 
 module.exports = run;
